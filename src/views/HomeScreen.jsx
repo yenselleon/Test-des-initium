bacc7fe909411fc6b78543d92ccec2458ba2af62
Yensel Leon <yensel41@gmail.com>
2023-04-26 08:17:44 -0400

    build
    new build

---
+++ src/views/HomeScreen.jsx
@@ -0,0 +1,14 @@
+/* import RegisterForm from "../components/RegisterForm"; */
+
+const HomeScreen = () => {
+    return (
+    <div className="bg-contact100" style={{ backgroundImage: "url('src/images/bg-01.jpg')" }}>
+        <div className="container-contact100">
+            {/* <UploadImageForm/> */}
+            <h1>Hola mundo</h1>
+        </div>
+    </div>
+    )
+}
+
+export default HomeScreen;

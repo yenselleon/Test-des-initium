6fdbdc0e84fa9aa78ca7ec8ba08895abdb8ece23
Yensel Leon <yensel41@gmail.com>
2023-04-26 08:38:32 -0400

    asd
    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import HomeScreen from './views/HomeScreen'
 
@@ -10,4 +12,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
--- src/views/HomeScreen.jsx
+++ src/views/HomeScreen.jsx
@@ -1,11 +1,11 @@
-/* import RegisterForm from "../components/RegisterForm"; */
+import RegisterForm from "../components/RegisterForm";
 
 const HomeScreen = () => {
     return (
     <div className="bg-contact100" style={{ backgroundImage: "url('src/images/bg-01.jpg')" }}>
         <div className="container-contact100">
             {/* <UploadImageForm/> */}
-            <h1>Hola mundo 23</h1>
+            <RegisterForm/>
         </div>
     </div>
     )

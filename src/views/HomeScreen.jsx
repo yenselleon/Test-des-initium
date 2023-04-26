dd6a3863c148d67022a8d32d9d596b50baf85386
Yensel Leon <yensel41@gmail.com>
2023-04-26 08:28:57 -0400

    asd
    asd

--- src/views/HomeScreen.jsx
+++ src/views/HomeScreen.jsx
@@ -5,7 +5,7 @@ const HomeScreen = () => {
     <div className="bg-contact100" style={{ backgroundImage: "url('src/images/bg-01.jpg')" }}>
         <div className="container-contact100">
             {/* <UploadImageForm/> */}
-            <h1>Hola mundo 2</h1>
+            <h1>Hola mundo 23</h1>
         </div>
     </div>
     )
